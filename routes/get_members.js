8e400035b1febe96cdd267b99f3c3ea69dd28459
Емельянова Д. О <emelyanovado@virtek.pro>
2023-04-12 16:02:00 +0500

    deploy-2

--- routes/get_members.js
+++ routes/get_members.js
@@ -1,12 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-const filepathMiddleware = require('../middleware/filepath.middleware')
 
 
 module.exports = function (app) {
     app.get('/list', (request, response) => {
         const path_to_members = path.join('members', 'members.txt')
-        console.log(__dirname)
 
         let data_members = fs.readFileSync(path_to_members, 'utf-8').replace(/}\,\s*$/g, '},{')
         data_members = '[' + data_members + ']'
