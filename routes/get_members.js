9f15124028684701a41af012f32c7c107e5d7e29
Емельянова Д. О <emelyanovado@virtek.pro>
2023-04-12 16:09:05 +0500

    deploy-3

--- routes/get_members.js
+++ routes/get_members.js
@@ -6,7 +6,8 @@ module.exports = function (app) {
     app.get('/list', (request, response) => {
         const path_to_members = path.join('members', 'members.txt')
 
-        let data_members = fs.readFileSync(path_to_members, 'utf-8').replace(/}\,\s*$/g, '},{')
+        let data_members = fs.readFileSync(path_to_members, 'utf-8').replace(/\,\s*$/g, '')
+        console.log(data_members)
         data_members = '[' + data_members + ']'
 
         response.status(200).send(data_members)
