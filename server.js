f47d599c708013d035a1cc0bd3ebe5353235683f
Емельянова Д. О <emelyanovado@virtek.pro>
2023-04-14 11:17:41 +0500

    commit3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 80
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*')
